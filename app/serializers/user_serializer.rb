540178fdb55989a3a3df01b7f360fc4336f965dd
jlangley3 <jlangleyiii@aol.com>
2020-07-28 10:45:24 -0400

    added gifts to contacts to events in users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -12,7 +12,17 @@ class UserSerializer < ActiveModel::Serializer
       object.events.each do |event|
         custom_event = event.attributes 
         contacts = event.contacts
- 
+        
+        cons =[]
+        contacts.each do |contact|
+          # custom_gifts = []
+          custom_contact = contact.attributes
+          gifts = contact.gifts.where(:event => event )
+                custom_contact[:gifts] = gifts
+
+            cons << custom_contact
+        end
+        
         gifts = []
         event.gifts.each do |gift|
           custom_gift = gift.attributes
@@ -21,10 +31,52 @@ class UserSerializer < ActiveModel::Serializer
         end
 
         custom_event[:gifts] = gifts
-        custom_event[:contacts] = contacts
+        custom_event[:contacts] = cons
         custom << custom_event
       end
       return custom
     end
 
 end
+
+
+
+
+# def events
+#   custom = []
+#   contacts = {}
+#   object.events.each do |event|
+#     custom_event = event.attributes 
+#     contacts = event.contacts
+
+#     gifts = []
+#     event.gifts.each do |gift|
+#       custom_gift = gift.attributes
+#       custom_gift[:contact] = gift.contact
+#       gifts << custom_gift
+#     end
+
+#     custom_event[:gifts] = gifts
+#     custom_event[:contacts] = contacts
+#     custom << custom_event
+#   end
+#   return custom
+# end
+
+
+
+
+
+
+
+# new_contacts = []
+# object.events.each do |event|
+#   contacts = []
+#   custom_event = event.attributes 
+#   contacts = event.contacts
+#    contacts.each do |contact|
+#     custom_contact = contact.attributes
+#      custom_contact[:gifts] = contact.gifts
+#       custom_contact[:contact] = custom_contact
+#          new_contacts << custom_contact
+#    end

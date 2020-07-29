0f850477ab60baa61effa5efb9a082b144d120d4
jlangley3 <jlangleyiii@aol.com>
2020-07-28 21:36:51 -0400

    fixed scontact serlial

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,25 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :start_date, :end_date, :budget, :repeating, :created_at, :user_id, :updated_at
+  attributes :id, :title, :start_date, :end_date, :budget, :repeating, :created_at, :user_id, :updated_at, :contacts
 
   has_many :gifts
   has_many :reminders
-  has_many :contacts, through: :gifts
+  # has_many :contacts, through: :gifts
+
+
+  def contacts
+    custom = []
+    contacts = []
+    object.contacts.each do |contact|
+      custom_contact = contact.attributes
+      gifts = contact.gifts.where(:event => object )
+      custom_contact[:gifts] = gifts
+      contacts << custom_contact
+    end
+    
+    custom << contacts
+
+      return custom
+  end
+  
+
 end
